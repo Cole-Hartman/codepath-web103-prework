37ee7880bf121a6920964d012b2eb4e583b7a117
Cole Hartman <119756359+Cole-Hartman@users.noreply.github.com>
2024-07-19 15:18:24 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cbrown: {
+          l: "#6f4f28", // Lighter shade
+          DEFAULT: "#543310", // Base color
+          d: "#3b1f0c", // Darker shade
+        },
+      },
+    },
   },
   plugins: [
     ({ addVariant }) => {
